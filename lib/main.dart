c339eca006b3debfca3b8bee140635628ae2c1cd
Xavier Berroa <xavilitoh@gmail.com>
2024-12-26 14:49:14 -0400

    elimina elementos no necesarios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:ladex/src/blocs/bloc_provider.dart';
 import 'package:ladex/src/pages/pokemon_details/pokemon_details_page.dart';
@@ -15,29 +13,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(      
+    return BlocProvider(
       child: MaterialApp(
         title: 'Pokedex',
         routes: {
           'home': (BuildContext context) => const HomePage(),
-          'pk_details':(context) => const PokemonDetailPage(),
-          
+          'pk_details': (context) => const PokemonDetailPage(),
         },
         theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Colors.blue,
           scaffoldBackgroundColor: Colors.white,
-          textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.black)),
+          textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.black)),
         ),
         darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.teal,
-          scaffoldBackgroundColor: Colors.black,
-          textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.white)),
-          cardTheme: const CardTheme(
-             color: Colors.black45
-          )
-        ),
+            brightness: Brightness.dark,
+            primaryColor: Colors.teal,
+            scaffoldBackgroundColor: Colors.black,
+            textTheme:
+                const TextTheme(bodyLarge: TextStyle(color: Colors.white)),
+            cardTheme: const CardTheme(color: Colors.black45)),
         themeMode: ThemeMode.system,
         home: const HomePage(),
       ),
